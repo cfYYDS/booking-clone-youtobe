5b1c118dbb7705229f6281ef4ee631edbcf0cdac
Slack <1048001021@qq.com>
2024-01-06 16:31:31 +0800

    finish trending section

---
+++ data/trending_data.ts
@@ -0,0 +1,39 @@
+export const trending_data = [
+    {
+        id:1,
+        src:'https://i.ytimg.com/vi/zpOULjyy-n8/maxresdefault.jpg',
+        title:'Dubai',
+        location:'United Arab Emirates',
+        description:'15 Deals',
+    }, {
+        id:2,
+        src:'https://i.ytimg.com/vi/zpOULjyy-n8/maxresdefault.jpg',
+        title:'Dubai',
+        location:'United Arab Emirates',
+        description:'15 Deals',
+    }, {
+        id:3,
+        src:'https://i.ytimg.com/vi/zpOULjyy-n8/maxresdefault.jpg',
+        title:'Dubai',
+        location:'United Arab Emirates',
+        description:'15 Deals',
+    }, {
+        id:4,
+        src:'https://i.ytimg.com/vi/zpOULjyy-n8/maxresdefault.jpg',
+        title:'Dubai',
+        location:'United Arab Emirates',
+        description:'15 Deals',
+    }, {
+        id:5,
+        src:'https://i.ytimg.com/vi/zpOULjyy-n8/maxresdefault.jpg',
+        title:'Dubai',
+        location:'United Arab Emirates',
+        description:'15 Deals',
+    }, {
+        id:6,
+        src:'https://i.ytimg.com/vi/zpOULjyy-n8/maxresdefault.jpg',
+        title:'Dubai',
+        location:'United Arab Emirates',
+        description:'15 Deals',
+    },
+]
